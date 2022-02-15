1805ab9b2a4d742864b6510c1a60301c57fe6efb
Eduard <occhipinti.eduard@icloud.com>
2022-02-15 17:19:08 +0100

    aaaaa

--- src/bus.cpp
+++ src/bus.cpp
@@ -1,23 +1,34 @@
 #include "headers/bus.hh"
 
-template <typename T>
-T read(struct bus_t<T> bus,int size){
-    printf("Bus.bitfield is %d\n", bus.bitfield);
-    printf("Size is %d\n", size);
-    return bus.bitfield >> size; // 10 0001 = 33
-}
+Bus::Bus()
+{
+    // auto fp = &read<uint32_t>;
+    /*BIOS_ROM =
+    {
+        0,
+        7,
+        0,
+        &read<uint32_t>,
+        &write<uint32_t>
+    };*/
 
-template <typename T>
-void write(struct bus_t<T> bus, int size, T data){
-    bus.bitfield = data;
-}
-
-Bus::Bus() {
+    BIOS_ROM.bitfield = 0;
     BIOS_ROM.r_perm = 7;
-    BIOS_ROM.w_perm = 0;
-    BIOS_ROM.read_ptr = read<uint32_t>;
+    BIOS_ROM.read_ptr = &Bus::read<uint32_t>;
+    BIOS_ROM.write_ptr = &Bus::write<uint32_t>;
+
+    printf("%p\n", (void *)BIOS_ROM.read_ptr);
+
+    read(BIOS_ROM, 4);
+    
+    //(this->BIOS_ROM->*read<uint32_t>)(BIOS_ROM, 4);
+
+    //lookup = {BIOS_ROM};
+    // BIOS_ROM.r_perm = 7;
+    // BIOS_ROM.w_perm = 0;
+    // BIOS_ROM.read_ptr = read<uint32_t>;
 }
 
-Bus::~Bus(){
-
+Bus::~Bus()
+{
 }
