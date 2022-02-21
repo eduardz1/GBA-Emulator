197e5bc8f7418eb48ba6971113d79637a4273b8c
GDennis01 <dennisistheway@gmail.com>
2022-02-21 23:21:32 +0100

    Decoded Data Processing instructions

--- src/arm7tdmi.cpp
+++ src/arm7tdmi.cpp
@@ -51,6 +51,7 @@ uint32_t Arm7tdmi::fetch(Bus bus_controller){
 
 void Arm7tdmi::decode_execute(Arm7tdmi::_instruction instruction, Bus bus_controller){
     union _instruction tmp;
+    uint8_t opcode;
     tmp.word = fetch(bus_controller);
 
     uint16_t opcode_id = 0;   
@@ -58,10 +59,124 @@ void Arm7tdmi::decode_execute(Arm7tdmi::_instruction instruction, Bus bus_contro
     opcode_id <<= 4;
     opcode_id |= tmp.opcode_id1;
 
+    opcode=(tmp.opcode_id2 & 30)>>1;//isolating opcode bits
+
     // 011x xxxx xxx1
     if((opcode_id >> 9 == 3) && ((opcode_id & 1) == 1))
         undefined_handler(); // undefined instruction
 
+    if(tmp.opcode_id2>>6 == 0){//bit 26-27 unset
+        if(tmp.opcode_id2 ^ 32 == 0 || !(tmp.opcode_id1 & 1 == 0) || !(tmp.opcode_id1 & 8 == 0)){//bit 25 set or bit 4 unset  or bit 7 unset
+            switch(opcode){
+                case 0x0:AND(instruction.word); break;
+                case 0x1:EOR(instruction.word); break;
+                case 0x2:SUB(instruction.word); break;
+                case 0x3:RSB(instruction.word); break;
+                case 0x4:ADD(instruction.word); break;
+                case 0x5:ADC(instruction.word); break;
+                case 0x6:SBC(instruction.word); break;
+                case 0x7:RSC(instruction.word); break;
+                case 0x8:{
+                    if(tmp.opcode_id2&1){//bit 20 set
+                        TST(instruction.word); 
+                    }else{
+                        if((tmp.word>>16)&15 == 1 && ((tmp.word>>12)==0) && (tmp.opcode_id2&32)==0 ){//bit[16:19] set and bit[0:11] unset and bit[25] unset
+                            MRS(instruction.word);
+                        }else{
+                            undefined_handler();
+                        }
+                    }
+                    break;
+                }
+                case 0x9:{
+                    if(tmp.opcode_id2 & 1){// bit 20 set(S flag == 1)
+                        TEQ(instruction.word);
+
+                    }else{//bit 20 unset(S flag == 0)
+                        if(tmp.opcode_id2 & 32 == 0){//bit 25 set(I flag == 1)
+                            if((tmp.word>>12)&15==15){//bit[12:15] set
+                                MSR(instruction.word);
+                            }else 
+                                undefined_handler();
+                        }else{//bit 25 unset(I flag == 0)
+                            if(tmp.opcode_id1 == 0){//bit[4:7] unset
+                                if((tmp.word >>12)&15 == 15 && (tmp.word >>12)&15 == 0){//bit[12:15] set and bit[8:11] unset
+                                    MSR(instruction.word);
+                                }else
+                                    undefined_handler();
+                            }else if(tmp.opcode_id1 == 1){
+                                    if((tmp.word>>8)&4095 == 4095){//bit[8:19] set
+                                        BX(instruction.word);
+                                    }else
+                                        undefined_handler();
+                            }else 
+                                undefined_handler();
+                        }
+                    }
+                    break;
+                }
+                case 0xA:{
+                    if((tmp.opcode_id2&1)==1){//bit 20 set(S flag == 1)
+                        if((tmp.word>>12)&15 == 0){//bit[12:15] unset
+                            CMP(instruction.word);
+                        }else{
+                            undefined_handler();
+                        }
+                    }else{//bit 20 unset(S flag == 0)
+                        if((tmp.word>>16)&15==1 && (tmp.word&4095) == 0 && (tmp.opcode_id2&32)==0){//bit[16:19] set and bit[0:11] unset and bit[25] unset
+                            MRS(instruction.word);
+                        }else 
+                            undefined_handler();
+                    }
+                   
+                    break;
+                }
+                case 0xB:{
+                    if((tmp.opcode_id2&1)==1){//bit 20 set(S flag == 0)
+                        if(((tmp.word>>12) & 15) == 0){//bit[12:15] unset
+                            CMN(instruction.word);
+                        }else{
+                            undefined_handler();
+                        }
+                    }else{//bit 20 unset(S flag == 1)
+                        if((tmp.opcode_id2&32) == 1){//bit 25 set(I flag == 1)
+                            if(((tmp.word>>12) & 15) == 1){
+                                MSR(instruction.word);
+                            }else{
+                                undefined_handler();
+                            }
+                        }else{//bit 25 unset (I flag == 0)
+                            if(tmp.opcode_id1 == 0 && ((tmp.word>>12)&15 ==1) && (tmp.word>>8)&15 == 0){//bit [4:7] unset and bit[12:15] set and bit[8:11] unset
+                                MSR(instruction.word);
+                            }else{
+                                undefined_handler();
+                            }
+                        }
+                    }
+                    break;
+                
+                }
+                case 0xC:ORR(instruction.word); break;
+                case 0xD:{
+                    if(((tmp.word>>16) & 15)==0){//bit[16:19] unset
+                        MOV(instruction.word);
+                    }else{
+                        undefined_handler();
+                    }
+                    break;
+                }
+                case 0xE:BIC(instruction.word); break;
+                case 0xF:{
+                     if(((tmp.word>>16) & 15)==0){//bit[16:19] unset
+                        MVN(instruction.word);
+                    }else{
+                        undefined_handler();
+                    }
+                    break;
+                }
+            }
+        }
+    }
     switch(tmp.opcode_id1)
     {
     case 0x1:
